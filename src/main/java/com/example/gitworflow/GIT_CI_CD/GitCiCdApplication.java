9729aa5c0a59cc99998ae9c48eeb13952249e02e
hems <hems07293@gmail.com>
2026-02-13 15:43:04 +0530

    Initial commit - Spring Boot project

---
+++ src/main/java/com/example/gitworflow/GIT_CI_CD/GitCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitworflow.GIT_CI_CD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitworflow/GIT_CI_CD/GitCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitworflow.GIT_CI_CD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
